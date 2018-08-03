1687990814458012ee9304028bfd992f8e26af75
Jim Yang <jyang_123@hotmail.com>
2018-08-03 12:30:32 -0400

    Finished database

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const models = require('./models');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+
+models.db.authenticate().then(() => {
+    console.log('connected to the database');
+});
 
 app.get("/", async (req, res, next) => {
     res.send(`
@@ -22,6 +27,11 @@ app.get("/", async (req, res, next) => {
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`app listening to port ${PORT}`);
-});
+async function init() {
+    await models.db.sync({force: true});
+    app.listen(PORT, () => {
+        console.log(`app listening to port ${PORT}`);
+    });
+}
+
+init();
